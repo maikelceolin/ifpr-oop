16dbf96ae8738e351b6f13ceb7b5437ccd1f2bad
maikel <maikelceolin@gmail.com>
2022-09-27 20:58:52 -0300

    sync

--- empresa.py
+++ empresa.py
@@ -1,14 +1,11 @@
 import funcionario as func
-
+__func
 class Empresa():
-    def __init__(self, codigo, nome, end, cnpj, cpf, func):
+    def __init__(self, codigo, nome)
         self.__codigo = codigo
         self.__nome = nome
         self.__end = end
-        self.__cnpj = cnpj
-        self.__cpf = func.Funcionario.cpf
-        self.__func = func
-
+    
     def describe(self):
         print(f"\nCodigo: {self.__codigo}\nNome: {self.nome}\nEndereço:{self.__end}\nCPNJ:{self.__cnpj}\n")
 
@@ -17,8 +14,13 @@ class Empresa():
 
 #contratar  
 
-    #pedir dados
-#associacao, agragação e heranç
+#pedir dados
+#associacao, agragação e herança
 #exibir lista de funcionarios
 
-
+    def contratar(self):
+        nome = input()
+        cpf = input
+        setor = input
+        salario = input
+        self.__func
