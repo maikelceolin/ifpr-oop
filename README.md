2718d37fc10808bc97fcc1455c02011f2553ced3
maikel <maikelceolin@gmail.com>
2022-10-04 14:58:52 -0300

    sync

---
+++ README.md
@@ -0,0 +1,14 @@
+Este programa realiza o cadastro de funcionários em um arquivo excel e consulta a planilha
+Para funcionar corretamente, precisa do seguinte:
+
+*Instalar lib openpyxl:
+
+    Linux: sudo pip install openpyxl
+        Obs.: verificar se o python está no PATH.
+
+    Windows: pip install openpyxl
+    Mac: pip3 install openpyxl
+
+1. A planilha esta vazia, assim, é necessário antes contratar algum funcionário.
+2. Escolhi deixar as classes de "cargos" dentro de funcionario.py
+3. Executar arquivo main
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from funcionario import Empresa, Administrador, Vendedor, Operario
-from programa import Menu
+from openpyxl import load_workbook
+from funcionario import Administrador, Vendedor, Operario
+from menu import Menu
 
 Menu.titulo(None)
 
@@ -7,36 +8,44 @@ while True:
     op = Menu.principal(None)
     if op == "1":
         
-        i = [1] # este i serve para controlar as linhas do excel, a cada laço, desce uma linha
+        i = [1] # este i serve para controlar as tabelas do excel, a cada laço, desce uma tabela
         while True:
             cargo = Menu.cargo(None)
-    
-            if cargo == "1":
-                i.append(1)
-                vendedor = Vendedor()
-                vendedor.contratar(i)
-                
-
-            elif cargo == "2":
-                i.append(1)
-                operario = Operario()
-                operario.contratar(i)
-                
-
-            elif cargo == "3":
-                i.append(1)
-                adm = Administrador()
-                adm.contratar(i)
-                
-            elif cargo == "0":
+            if cargo == "0":
                 break
 
             else:
-                print("\n**** Opção inválida")
+                i.append(1) #adiciona "1" na lista para contabilizar registros na planilha
+                if cargo == "1":
+                    vendedor = Vendedor()
+                    vendedor.contratar(i)
+                    
+                elif cargo == "2":
+                    operario = Operario()
+                    operario.contratar(i)
+                    
+                elif cargo == "3":
+                    adm = Administrador()
+                    adm.contratar(i)
 
     elif op =="2":
-        print("Voce ainda nao fez nada aqui\n")
-        
+        planilha = load_workbook("./cadastro.xlsx")
+        tabela = planilha.active
+        for i in range(2,tabela.max_row+1):
+            print("-"*50)
+            print("NOME: {}".format(tabela['A'+ str(i)].value),end="   ")
+            print("CPF: {}".format(tabela['B'+ str(i)].value),end="   ")
+            print("SETOR: {}".format(tabela['C'+ str(i)].value))
+            print("ENTRADA: {}".format(tabela['D'+ str(i)].value),end="   ")
+            print("SAÍDA: {}".format(tabela['E'+ str(i)].value), end="    ")
+            print("CARGO: {}".format(tabela['F'+ str(i)].value))
+            print("SALÁRIO: {}".format(tabela['G'+ str(i)].value),end="   ")
+            print("AUXÍLIO: {}".format(tabela['H'+ str(i)].value),end="   ")
+            print("VENDAS: {}".format(tabela['I'+ str(i)].value))
+            print("PRODUÇÃO: {}".format(tabela['J'+ str(i)].value),end="   ")
+            print("COMISSÃO: {}".format(tabela['K'+ str(i)].value),end="   ")
+            print("REMUNERAÇÃO: {}".format(tabela['L'+ str(i)].value))
+            
     else:
         print("\n**** Opção inválida")
                 
